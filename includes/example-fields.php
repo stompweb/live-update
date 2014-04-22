fc39e30927cd7b718e0d20e8abf3fd285e2c92a9
Steven Jones <stevenjones@Stevens-iMac-6.local>
2014-04-22 10:58:56 +0100

    Updated example fields.

--- includes/example-fields.php
+++ includes/example-fields.php
@@ -5,7 +5,7 @@ function my_meta_box_array($meta_boxes) {
     $meta_boxes = array();
 
     $fields = array(
-        array( 'type' => 'title', 'title' => 'Circuit Title', 'selector' => 'h1.entry-title'),
+        array( 'type' => 'title', 'title' => 'Post Title', 'selector' => 'h1.entry-title'),
         array( 'type' => 'text', 'id' => 'my_post_meta', 'title' => 'Please update me', 'selector' => '.my-post_meta'),
     );
 
